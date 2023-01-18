cdf389cbe4ae4435962b40648df1b5865154428b
jonn-pc <joliversalamanca@gmail.com>
2023-01-18 12:31:48 -0400

    add el Promise.all

--- ajax-ejercicios/stripe-checkout/assents/stripe-checkout.js
+++ ajax-ejercicios/stripe-checkout/assents/stripe-checkout.js
@@ -4,16 +4,24 @@ const $tacos = document.getElementById("tacos");
 const $template = document.getElementById("taco-template");
 const $fragment = document.createDocumentFragment();
 
-fetch("https://api.stripe.com/v1/products",{
+const fetchOption ={
+    
     headers:{
-        Authorization:`Bearer ${STRIPE_KEYS.secret}`,
+        Authorization:`Bearer ${STRIPE_KEYS.secret}`
     }
-})
-.then((res)=>{
-    console.log(res);
-    return res.json();
-})
-.then((json)=>{
-    console.log(json);
-})
+};
 
+let products, prices;
+
+Promise.all([
+    fetch("https://api.stripe.com/v1/products",fetchOption),    
+    fetch("https://api.stripe.com/v1/prices",fetchOption)
+])
+.then(todasLasRespuestas =>Promise.all(todasLasRespuestas.map(respuestaIndividual => respuestaIndividual.json())))
+.then(jsonRespuesta => {
+    // console.log(jsonRespuesta);
+    products = jsonRespuesta[0].data;
+    prices = jsonRespuesta[1].data;
+    console.log(products, prices);
+})
+.catch()
